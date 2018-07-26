156017640aba218a5783059698d0994225d971e0
Sean <omerta201@yahoo.com>
2018-07-26 14:53:27 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require("express"),
+    app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.get("/", function (req, res) {
+    res.render("index");
+});
+app.get("/menu", function (req, res) {
+    res.render("menu");
+});
+app.get("/book", function (req, res) {
+    res.render("book");
+});
+app.get("/contact", function (req, res) {
+    res.render("contact");
+});
+app.listen(3000, function () {
+    console.log("Server is live on Port 3000");
+});
